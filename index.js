44e22b05cbaa99639ddbca622b9b96ba5ba0e1af
unknown <nishavkumar07@gmail.com>
2025-08-03 21:36:37 +0530

    final push i think

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 require('dotenv').config();
 const axios = require('axios');
 const app = express();
-const port = 6969;
+const port = process.env.PORT || 3000;
 const path = require("path");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
