71e90370e282cd9349c608dcc26bb84794d973e4
unknown <nishavkumar07@gmail.com>
2026-02-01 19:15:52 +0530

    fixing 400 err

--- test.js
+++ test.js
@@ -1,4 +1,10 @@
+const ai = require("./ai.js");
+const mainfx = require(ai.reply);
 const prompt  = require('./prompt.js');
 
 const data = prompt.absuivePrompt("friendly","h");
 console.log(data);
+
+async function test(prompt) {
+    
+}
